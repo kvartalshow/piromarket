ee5d833901a2b6ff12876b7c8075cf72184d33b1
Pavel Grinchenko <kvartalshow@gmail.com>
2020-10-07 18:30:13 +0300

    Add pirotech-block

---
+++ js/jacor.js
@@ -0,0 +1,11 @@
+< script type = "text/javascript" >
+    $(document).ready(function () {
+        $("#change").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({
+                scrollTop: top
+            }, 1500);
+        })
+    };); < /script>
